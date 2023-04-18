930e9c0827fa205f2c67118afe6b5fdc59180995
wilhelm-christensson <w.christensson@gmail.com>
2023-04-18 12:31:47 +0200

    iOS Firebase configuration
    Added iOS configuration and compatability with Firebase and Flutterfire plugin

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:surfs_up_app/main.dart';
+import 'package:surfs_up/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
