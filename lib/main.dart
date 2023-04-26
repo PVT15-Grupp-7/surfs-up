b9d8ae6d6f15b96049795fd7d477d0a7ecdc6eb8
HazelGrouse1027 <102599992+HazelGrouse1027@users.noreply.github.com>
2023-04-26 12:06:13 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'services/firebase_options.dart';
 
+// testkommentar
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
