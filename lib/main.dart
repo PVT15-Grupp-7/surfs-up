c844fa8c8c8b9150959ac0476950a589e12ce85e
wilhelm-christensson <w.christensson@gmail.com>
2023-05-04 11:23:50 +0200

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:surfs_up/pages/navigation_admin.dart';
 import 'package:surfs_up/pages/wrapper.dart';
 import 'package:surfs_up/services/authentication_service.dart';
 import 'data/app_user_data.dart';
@@ -52,16 +51,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-
-/*
- @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const NavigationAdmin(),
-      theme: themeData(),
-    );
-  }
- */
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,13 +9,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
     return Scaffold(
       backgroundColor: const Color(0xFF132246),
       appBar: AppBar(
