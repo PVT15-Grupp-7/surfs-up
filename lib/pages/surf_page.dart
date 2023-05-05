9ff601b2256d0dc7896361cb1e15fe59ab8f2e3a
AugustAfola <119737433+AugustAfola@users.noreply.github.com>
2023-05-05 12:48:30 +0200

    Påbörjad surfsida
    tillfällig

--- lib/pages/surf_page.dart
+++ lib/pages/surf_page.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:surfs_up/shared/widgets/app_bar.dart';
+import 'package:surfs_up/shared/constants/dynamic_content.dart';
+import 'package:surfs_up/shared/widgets/weather_box.dart';
+
+//import 'package:surfs_up/shared/widgets/weather_box.dart';
+
+List<String> list = <String>[
+  'Torö',
+  'Väddö',
+];
 
 class SurfPage extends StatelessWidget {
   const SurfPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF132246),
+    return Scaffold(
+      backgroundColor: const Color(0xFF132246),
       body: Center(
-        child: Text(
-          'Surf Page',
-          style: TextStyle(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            SizedBox(height: 50),
+            Text(
+              'Weather Page',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            WeatherBox(),
+            SizedBox(height: 20),
+            WeatherBox(),
+            SizedBox(height: 20),
+            WeatherBox(),
+          ],
         ),
       ),
     );
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:surfs_up/shared/widgets/weather_box.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF132246),
+    return Scaffold(
+      backgroundColor: const Color(0xFF132246),
       body: Center(
-        child: Text(
-          'Weather Page',
-          style: TextStyle(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            SizedBox(height: 50),
+            Text(
+              'Weather Page',
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            WeatherBox(),
+            SizedBox(height: 20),
+            WeatherBox(),
+            SizedBox(height: 20),
+            WeatherBox(),
+          ],
         ),
       ),
     );
