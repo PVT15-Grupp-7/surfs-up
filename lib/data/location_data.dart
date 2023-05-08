6f1d85021c170a32476e081755a1174d0f208c3a
wilhelm-christensson <w.christensson@gmail.com>
2023-05-08 14:27:07 +0200

    Updated safety pages

--- lib/data/location_data.dart
+++ lib/data/location_data.dart
@@ -2,17 +2,19 @@ class Location {
   final String name;
   final String address;
   final String description;
-  final String imagePath;
+  final String infoImagePath;
   final String seabed;
   final String defibrillatorURL;
+  final String defibrillatorImagePath;
 
   Location(
       {required this.name,
       required this.address,
       required this.description,
-      required this.imagePath,
+      required this.infoImagePath,
       required this.seabed,
       required this.defibrillatorURL,
+      required this.defibrillatorImagePath,
       });
 }
 
@@ -21,16 +23,18 @@ final List<Location> locations = [
     name: 'Torö',
     address: '114 92 Nynäshamn',
     description: 'The birthplace of surfing',
-    imagePath: 'assets/images/surfs_up_logo.png',
+    infoImagePath: 'assets/images/surfs_up_logo.png',
     seabed: 'Stone',
     defibrillatorURL: 'https://www.hjartstartarregistret.se/#/30499/',
+    defibrillatorImagePath: 'assets/images/defibrillatorn_torö.png',
   ),
   Location(
     name: 'Väddö',
     address: '760 40 Väddö',
     description: 'Home to some of the best surf spots in the world',
-    imagePath: 'assets/images/surfs_up_logo.png',
+    infoImagePath: 'assets/images/surfs_up_logo.png',
     seabed: 'Sand',
-    defibrillatorURL: 'https://www.hjartstartarregistret.se/#/30499/',
+    defibrillatorURL: 'https://www.hjartstartarregistret.se/#/82423/',
+    defibrillatorImagePath: 'assets/images/defibrillatorn_väddö.png',
   ),
 ];
