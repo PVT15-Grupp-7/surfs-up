160711b805cc8facc021720972a259d10e622f19
linusahl <liah0501@student.su.se>
2023-05-25 11:31:38 +0200

    updated surf page

--- lib/pages/surf_page.dart
+++ lib/pages/surf_page.dart
@@ -15,84 +15,96 @@ class SurfPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-          itemCount: listOfDayWeatherData.length,
-          itemBuilder: (_, index) {
-            final item = listOfDayWeatherData[index];
-            final DateTime date = item.first.date;
-            final DateFormat dateFormat = DateFormat('EEE, MMM d');
-            final bool isToday = (index == 0);
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Card(
-                // key: PageStorageKey(item['id']),
-                color: kMediumBlue,
-                elevation: 4,
-                child: ExpansionTile(
-                  iconColor: Colors.white,
-                  collapsedTextColor: Colors.white,
-                  collapsedIconColor: Colors.white,
-                  childrenPadding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                  ),
-                  expandedCrossAxisAlignment: CrossAxisAlignment.end,
-                  title: Text(
-                    isToday ? "Today" : dateFormat.format(date),
-                    style: CustomTextStyle.title3,
-                  ),
-                  subtitle: RowWidget(dayData: listOfDayWeatherData[index]),
-                  tilePadding: const EdgeInsets.all(11),
-                  textColor: Colors.white,
-                  children: item.map((WeatherData hourItem) {
-                    return Container(
-                      width: 500,
-                      decoration: BoxDecoration(
-                        color: kDarkBlue,
-                        border: Border.all(
-                          color: kTransparentGrey,
-                          width: 1,
+    if (listOfDayWeatherData[0].isNotEmpty) {
+      return Scaffold(
+        body: ListView.builder(
+            itemCount: listOfDayWeatherData.length,
+            itemBuilder: (_, index) {
+              final item = listOfDayWeatherData[index];
+              final DateTime date = item.first.date;
+              final DateFormat dateFormat = DateFormat('EEE, MMM d');
+              final bool isToday = (index == 0);
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Card(
+                  // key: PageStorageKey(item['id']),
+                  color: kMediumBlue,
+                  elevation: 4,
+                  child: ExpansionTile(
+                    iconColor: Colors.white,
+                    collapsedTextColor: Colors.white,
+                    collapsedIconColor: Colors.white,
+                    childrenPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                    ),
+                    expandedCrossAxisAlignment: CrossAxisAlignment.end,
+                    title: Text(
+                      isToday ? "Today" : dateFormat.format(date),
+                      style: CustomTextStyle.title3,
+                    ),
+                    subtitle: RowWidget(dayData: listOfDayWeatherData[index]),
+                    tilePadding: const EdgeInsets.all(11),
+                    textColor: Colors.white,
+                    children: item.map((WeatherData hourItem) {
+                      return Container(
+                        width: 500,
+                        decoration: BoxDecoration(
+                          color: kDarkBlue,
+                          border: Border.all(
+                            color: kTransparentGrey,
+                            width: 1,
+                          ),
                         ),
-                      ),
-                      child: ListTile(
-                        leading: Text('${hourItem.date.hour}:00'),
-                        title: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            ExpandedItem(
-                              itemIcon: Icons.air_outlined,
-                              itemText:
-                                  '${hourItem.windSpeed}m/s (${hourItem.gust})',
-                            ),
-                            ExpandedItem(
-                              itemIcon: hourItem.windIcon,
-                              itemText: hourItem.windDirectionSymbol,
-                            ),
-                            hourItem.surfIcon,
-                          ],
+                        child: ListTile(
+                          leading: Text('${hourItem.date.hour}:00'),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ExpandedItem(
+                                itemIcon: Icons.air_outlined,
+                                itemText:
+                                    '${hourItem.windSpeed}m/s (${hourItem.gust})',
+                              ),
+                              ExpandedItem(
+                                itemIcon: hourItem.windIcon,
+                                itemText: hourItem.windDirectionSymbol,
+                              ),
+                              hourItem.surfIcon,
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }).toList(),
+                      );
+                    }).toList(),
+                  ),
                 ),
-              ),
-            );
-          }),
-      floatingActionButton: Stack(
-        children: const [
-          Positioned(
-            bottom: 16.0,
-            right: 16.0,
-            child: InfoButtonClass(),
-          ),
-          Positioned(
-            bottom: 16.0,
-            left: 16.0,
-            child: ShareButtonClass(),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+              );
+            }),
+        floatingActionButton: Stack(
+          children: const [
+            Positioned(
+              bottom: 16.0,
+              right: 16.0,
+              child: InfoButtonClass(),
+            ),
+            Positioned(
+              bottom: 16.0,
+              left: 16.0,
+              child: ShareButtonClass(),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      );
+    } else {
+      return const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+            child: Text(
+          'Something went wrong, could not load data for from SMHI and YR.\n\n'
+          'Please try again later.',
+          textAlign: TextAlign.center,
+        )),
+      );
+    }
   }
 }
