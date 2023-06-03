2e4e388885d5b0f2c906df9dc86e51b5a79caa5c
wilhelm-christensson <w.christensson@gmail.com>
2023-06-03 11:45:39 +0200

    Dependency update

--- test/AuthenticationTests/user_repository_test.dart
+++ test/AuthenticationTests/user_repository_test.dart
@@ -4,8 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
 
-//denna lösning är en cookie-cutter från en yt-video men där implementering i Authservice är annorlunda och därför inte funkar
-
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 class MockFirebaseUser extends Mock implements User {}
@@ -19,7 +17,7 @@ void main() {
     return user;
   });
 
-  AuthenticationService userRepo = AuthenticationService.instance(auth: auth);
+  AuthenticationService userRepo = AuthenticationService();
 
   group('user repoistory test', () {
     when(auth.signInWithEmailAndPassword(email: "email", password: "password"))
@@ -31,7 +29,7 @@ void main() {
       bool signedIn =
           await userRepo.signInWithEmailAndPassword("email", "password");
       expect(signedIn, true);
-      expect(repo.status, Status.Authenticated);
+      expect(userRepo.user.isEmpty, false);
     });
 
     test("Sign out with google", () async {});
